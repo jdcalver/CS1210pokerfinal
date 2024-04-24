39cd0a806fd17dc9737f1a1fe68629e7afa23f5f
patrickaber2 <167939743+patrickaber2@users.noreply.github.com>
2024-04-24 19:52:09 -0400

    Add files via upload

--- odds.py
+++ odds.py
@@ -79,7 +79,38 @@ def odds(x, y):
                 #print("A two pair is not possible")
         prob = ((47 - totalCard) * 4) / 100
         return(f"The probability of hitting another {needed} is {prob}%")
+    #COME BACK TO THIS
         
+    if hand == "Three of a kind":
+        needed = ""
+        card1 = 0
+        card2 = 0
+        totalCard = 0
+        if x[0] == y[0]:
+            totalCard += 2
+            for val in community_cards:
+                if val[0] == x[0] or val[0] == y[0]:
+                    print("You have a three of a kind!")
+                elif val[0] != x[0] or val [0] != y[0]:
+                    return (2 / 47) * 100
+        if x[0] != y[0]:
+            for val in community_cards:
+                if val[0] == x[0]:
+                    card1 += 1
+                elif val[0] == y[0]:
+                    card2 += 1
+            if card1 >= 3 or card2 >= 3:
+                print("You have a three of a kind")
+            else:
+                if card1 > card2:
+                    needed = x[0]
+                    totalCard = card1
+                elif card1 < card2:
+                    needed = y[0]
+                    totalCard = card2
+        prob = ((4 - totalCard) / 47) * 100
+        return(f"The probability of hitting another {needed} is {prob:.2f}%")
+                
             
             
             
